a7060ec4d021a554da49e74e849e0aaca26b7015
Aashish Nehete <ashnehete@gmail.com>
2017-03-20 16:43:26 +0530

    Issue routes

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Routes
 var book_routes = require('./routes/book_routes');
 var user_routes = require('./routes/user_routes');
+var issue_routes = require('./routes/issue_routes');
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/library');
@@ -20,6 +21,7 @@ app.get('/', function (req, res) {
 
 app.use('/book', book_routes);
 app.use('/user', user_routes);
+app.use('/issue', issue_routes);
 
 app.listen(3000, function () {
     console.log('Running at port 3000...');
